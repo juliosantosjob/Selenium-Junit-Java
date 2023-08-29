947e89608aee688c4bd2fc3efd8d85a59eee95ff
Julio Santos <julio958214@gmail.com>
2023-08-28 21:18:29 -0300

    Adjusting code

--- scripts/installChrome.sh
+++ scripts/installChrome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -ex
+# https://www.linkedin.com/pulse/running-selenium-web-tests-github-actions-moataz-nabil/
 
-sudo apt update
+set -ex
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 google-chrome --version
